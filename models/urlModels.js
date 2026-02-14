4e037984a05dd46e3588bd7f141723a325438f65
Olumite <Michealizzzy.com>
2026-02-14 05:42:42 +0100

    Es modules

--- models/urlModels.js
+++ models/urlModels.js
@@ -1,9 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
 
 const filePath = path.join(__dirname, '../urls.json'); 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const saveUrl = (newUrlData) => {
     try {
