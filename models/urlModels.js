7239414030d130dff2009b162589f113f4bdb5e9
Sodeeq Olalekan Adeosun <54525417+Technology-World@users.noreply.github.com>
2026-02-14 16:10:18 +0100

    Updated get all function from EMS to CJS
    * created get all url function
    
    * Update urlModel.js from EMS to CJS
    
    * Add URL model with function to retrieve URLs using CJS
    
    * Delete models/urlModel.js

---
+++ models/urlModels.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '../data/urls.json');
+function getAllUrls() {
+    try {
+
+        if (!fs.existsSync(filePath)) {
+            return [];
+        }
+
+        const data = fs.readFileSync(filePath, 'utf8');
+
+        if (!data.trim()) {
+            return [];
+        }
+
+        return JSON.parse(data);
+
+    } catch (error) {
+        console.error("Error reading URLs:", error);
+        return [];
+    }
+}
+
+
+module.exports = { getAllUrls };
