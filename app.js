6ca516f07c79666b92bc18a8d1659906991e1a64
Olumite <Michealizzzy.com>
2026-02-14 14:53:53 +0100

    url modification

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const { findUrl } = require('./models/urlModels');
 
+const { shortenUrl } = require('./controllers/urlController');
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Form submission endpoint
-app.post('/shorten', (req, res) => {
-  const { longUrl } = req.body;
 
-  // Generate a short code
-  const code = Math.random().toString(36).substring(2, 8);
-
-  // Step 5: use findUrl() to save new mapping
-  const saved = findUrl({ code, longUrl });
-
-  res.send(`Short URL code: ${saved.code}`);
-});
+app.post('/shorten', shortenUrl)
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
